ad7a08ee47c01354843796cb0a521c503969bc6e
hu-po <hugo.p.cmu@gmail.com>
2024-06-06 16:31:24 -0700

    no egl

--- docker/test_docker.sh
+++ docker/test_docker.sh
@@ -1,3 +1,3 @@
 docker build -t loco-mujoco:test -f docker/Dockerfile.eval .
 docker run -it --rm loco-mujoco:test \
-    python3 /src/examples/replay_datasets/replay_Unitree.py
+    python3 /src/examples/replay_datasets/replay_Unitree_H1.py
--- examples/replay_datasets/replay_Unitree_H1.py
+++ examples/replay_datasets/replay_Unitree_H1.py
@@ -9,7 +9,17 @@ def experiment(seed=0):
 
     mdp = LocoEnv.make("UnitreeH1.walk.real")
 
-    mdp.play_trajectory_from_velocity(n_episodes=3, n_steps_per_episode=500)
+    mdp.play_trajectory_from_velocity(
+        n_episodes=3,
+        n_steps_per_episode=500,
+        record=True,
+        # videos/stompy/video.mp4
+        recorder_params=dict(
+            path="test_video", 
+            tag="stompy",
+            video_name="video",
+        ),
+    )
 
 
 if __name__ == '__main__':
